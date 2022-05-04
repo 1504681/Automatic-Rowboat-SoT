632eab5495799292c1e49b196f69d69755fd333f
Thomas Kelly <84855585+1504681@users.noreply.github.com>
2022-05-03 23:37:57 -0400

    added current active window check with win32gui

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pydirectinput as pdi
 import keyboard
 import time
 import random
+from win32gui import GetWindowText, GetForegroundWindow
+
 
 pdi.PAUSE = 0
 
@@ -30,27 +32,28 @@ States = StateMachine()
 
 toggled = False
 while True:
-    if keyboard.is_pressed('['):
-        toggled = False
-        States.state = None
-        pdi.keyUp('a')
-        pdi.keyUp('d')
-        print("Toggled off")
-    if keyboard.is_pressed(']'):
-        toggled = True
-    if toggled:
-        if States.state == None:
-            print("Rowing the boat...")
-            pdi.keyDown('a')
-            pdi.keyDown('d')
-            RowTimer.SetTimer(2+random.uniform(-0.1, 0.25))
-            States.state = "Rowing"
-        if States.state == "Rowing":
-            if RowTimer.Timer():
-                pdi.keyUp('a')
-                pdi.keyUp('d')
-                States.state = "Waiting"
-                RowTimer.SetTimer(0.47 + random.uniform(0, 0.3))
-        if States.state == "Waiting":
-            if RowTimer.Timer():
-                States.state = None
+    if GetWindowText(GetForegroundWindow()) == "Sea of Thieves":
+        if keyboard.is_pressed('['):
+            toggled = False
+            States.state = None
+            pdi.keyUp('a')
+            pdi.keyUp('d')
+            print("Toggled off")
+        if keyboard.is_pressed(']'):
+            toggled = True
+        if toggled:
+            if States.state == None:
+                print("Rowing the boat...")
+                pdi.keyDown('a')
+                pdi.keyDown('d')
+                RowTimer.SetTimer(2+random.uniform(-0.1, 0.25))
+                States.state = "Rowing"
+            if States.state == "Rowing":
+                if RowTimer.Timer():
+                    pdi.keyUp('a')
+                    pdi.keyUp('d')
+                    States.state = "Waiting"
+                    RowTimer.SetTimer(0.47 + random.uniform(0, 0.3))
+            if States.state == "Waiting":
+                if RowTimer.Timer():
+                    States.state = None
